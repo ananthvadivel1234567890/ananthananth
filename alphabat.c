bae59c4e1e20aeb1046f629aa73cdca37b99f875
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-01-26 10:02:21 +0530

    Create alphabat.c

---
+++ alphabat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("%c is an alphabet.",B);
+    else
+        printf("%c is not an alphabet.",e);
+
+    return 0;
+}
